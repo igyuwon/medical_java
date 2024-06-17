7c28e1a5724ddbcfab6d1c927d5f7d4c85a43025
igyuwon <igyuwon60@gmail.com>
2024-06-17 18:18:44 +0900

    랜덤숫자 문제 추가

---
+++ p0617/src/p0617/C0617_17.java
@@ -0,0 +1,14 @@
+package p0617;
+
+public class C0617_17 {
+    public static void main(String[] args) {
+        // 1-100까지 랜덤숫자 10개를 생성해서 10개의 숫자의 합을 출력하시오.
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            int num = (int)(Math.random()*100)+1;
+            System.out.println("생성한 숫자 : "+num);
+            sum += num;
+        }
+        System.out.println("총합 : "+sum);
+    }
+}
